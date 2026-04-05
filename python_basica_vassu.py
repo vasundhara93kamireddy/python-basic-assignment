ce5e32115a30aff5ae36e13454709c05944d5f01
vasundhara93kamireddy <kamireddyvasundhra@gmail.com>
2026-04-05 13:30:28 +0530

    Add files via upload

---
+++ python_basica_vassu.py
@@ -0,0 +1,11 @@
+name="Vassu"
+age=30
+hight=5.10
+student=True
+print("Name:",name, "Age:", age, "Hight", hight, "Student:",student)
+print(f"Name:{name} | Age:{age}  | Hight:{hight} | Student:{student}")
+months=age*12
+days=age*365
+remainder=age%7
+raised=age**2
+print(f"My age in Months:{months} | Days:{days}  | Remainder:{remainder} | Raised:{raised}")
